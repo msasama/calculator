b1bb1c0c84ae3e253275f42d3d5b7c817ab64a47
Shazeb <shazebamaan47@gmail.com>
2026-03-01 02:26:43 +0530

    second version with optimized code

--- calc.js
+++ calc.js
@@ -2,32 +2,22 @@ let ops=["+","-","*"]
 let btns=document.querySelector('.btns')
 let disp=document.querySelector('.disp');
 let num=0,j=0;
-for(let i=1;i<=13;i++){
-    let div=document.createElement('div');
-    div.classList.add('cell');
-    div.style.width=`calc(100%/4)`
-    div.style.height=`calc(100%/4)`
-    if(i%4==0){
-        div.classList.add('op')
-        div.textContent=ops[j++];
-    }
-    else{
-    div.classList.add('num')
-    div.textContent=num++;
-    }
-    btns.appendChild(div);
-}
+let op_present=false;
 function createbtn(c1,str){
     let div=document.createElement('div')
     div.classList.add('cell')
     div.classList.add(c1);
     div.textContent=str;
     div.style.width=`calc(100%/4)`;
-div.style.height=`calc(100%/4)`;
-btns.appendChild(div);
+    div.style.height=`calc(100%/4)`;
+    btns.appendChild(div);
+}
+for(let i=1;i<=13;i++){
+    if(i%4==0) createbtn('op',ops[j++]);
+    else createbtn('num',num++);
 }
 createbtn('clear',"AC");
-createbtn('op','=');
+createbtn('eq','=');
 createbtn('op','/');
 function Calc(){
     this.methods={
@@ -49,9 +39,26 @@ let btn=document.querySelectorAll('.cell');
 btn.forEach((bt)=>{
     bt.addEventListener('click',()=>{
         let str;
-        if(bt.classList.contains('op')) str=disp.textContent+" "+bt.textContent+" ";
-        else str=disp.textContent+bt.textContent;
-        if(bt.textContent=="=") str=calc.solve(str);
+        if(bt.classList.contains('clear')){
+             str="";
+             op_present=false;
+        }
+        else if(bt.classList.contains('op')){
+            if(op_present==true) disp.textContent=calc.solve(disp.textContent);
+            str=disp.textContent+" "+bt.textContent+" ";
+            op_present=true;
+        }
+        else {//means that contains num . 
+            str=disp.textContent+bt.textContent;
+        }
+        if(bt.textContent=="="){
+            if(op_present){
+             str=calc.solve(disp.textContent);
+             op_present=false;
+            }
+            else str=disp.textContent;
+        }
         disp.textContent=str;
     })
 })
+
