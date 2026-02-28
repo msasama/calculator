7f2fcc6ff9727049032edeefa4405eb87a7211f0
Shazeb <shazebamaan47@gmail.com>
2026-03-01 03:11:09 +0530

    added divide by zero,decimal rounding. added handling of consecutive operator case and early = case.

--- calc.js
+++ calc.js
@@ -3,6 +3,7 @@ let btns=document.querySelector('.btns')
 let disp=document.querySelector('.disp');
 let num=0,j=0;
 let op_present=false;
+let eq_press=false;
 function createbtn(c1,str){
     let div=document.createElement('div')
     div.classList.add('cell')
@@ -24,37 +25,63 @@ function Calc(){
         "+":(a,b)=>a+b,
         "-":(a,b)=>a-b,
         "*":(a,b)=>a*b,
-        "/":(a,b)=>a/b,
+        "/":(a,b)=>b==0?"cmon bruhh...":a/b,
     }
     this.solve=function(str){
         let split=str.split(' ');
         a=+split[0];
         op=split[1];
         b=+split[2];
-        return this.methods[op](a,b);
+       let result = this.methods[op](a,b);
+        
+        // If it's the snarky error, just return it without rounding
+        if (result === "cmon bruhh...") return result; 
+        
+        // Round to 3 decimal places so it doesn't overflow
+        return Math.round(result * 1000) / 1000;
     }
 }
+
 let calc=new Calc();
+
 let btn=document.querySelectorAll('.cell');
 btn.forEach((bt)=>{
     bt.addEventListener('click',()=>{
+        if (disp.textContent === "cmon bruhh...") {
+            disp.textContent = "";
+            op_present = false;
+            eq_press = false;
+        }
         let str;
         if(bt.classList.contains('clear')){
              str="";
              op_present=false;
+             eq_press=false;
         }
         else if(bt.classList.contains('op')){
-            if(op_present==true) disp.textContent=calc.solve(disp.textContent);
-            str=disp.textContent+" "+bt.textContent+" ";
-            op_present=true;
+            // Check if the last character is a space (meaning an operator is already there waiting for a number)
+            if (disp.textContent.endsWith(" ")) {
+                let split = disp.textContent.split(' ');
+                str = split[0] + " " + bt.textContent + " "; // Swap the operator
+            } else {
+                if(op_present==true) disp.textContent=calc.solve(disp.textContent);
+                str=disp.textContent+" "+bt.textContent+" ";
+                op_present=true;
+            }
+            eq_press=false;
         }
         else {//means that contains num . 
+            if(eq_press==true) disp.textContent="";
             str=disp.textContent+bt.textContent;
+            eq_press=false;
         }
-        if(bt.textContent=="="){
-            if(op_present){
+       if(bt.textContent=="="){
+            let split = disp.textContent.split(' ');
+            // Only solve if op_present is true AND the second number isn't blank
+            if(op_present && split[2] !== ""){
              str=calc.solve(disp.textContent);
              op_present=false;
+             eq_press=true;
             }
             else str=disp.textContent;
         }
